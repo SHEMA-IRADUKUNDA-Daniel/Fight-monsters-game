ff639e9f8f48e2b76413883c41d1fad87097565d
danieliradukunda00@gmail.com <gad@Daniels-MacBook-Pro.local>
2024-08-13 14:29:43 +0200

    adding final js

--- script.js
+++ script.js
@@ -84,8 +84,8 @@ button2.onclick = goCave;
 button3.onclick = fightDragon;
 
 function update (location){
-    monsterStats.style.display = "none"
-    button1.innerText = location["button text"][0];
+  monsterStats.style.display = "none";
+  button1.innerText = location["button text"][0];
   button2.innerText = location["button text"][1];
   button3.innerText = location["button text"][2];
   button1.onclick = location["button functions"][0];
@@ -94,14 +94,15 @@ function update (location){
   text.innerHTML = location.text;
 }
 function goTown(){
-  update([locations[0]])
+  update(locations[0]);
 }
 function goStore(){
-  update([locations[2]])
+  update(locations[1]);
 }
 function goCave(){
-  update([locations[3]])
+  update(locations[2]);
 }
+
 function buyHealth(){
 
   if(gold >= 10){
@@ -200,4 +201,61 @@ function isMonsterHit() {
 
 function dodge() {
   text.innerText = "You dodge the attack from the " + monsters[fighting].name;
+}
+function defeatMonster() {
+  gold += Math.floor(monsters[fighting].level * 6.7);
+  xp += monsters[fighting].level;
+  goldText.innerText = gold;
+  xpText.innerText = xp;
+  update(locations[4]);
+}
+function lose(){
+  update([locations[5]]);
+}
+function winGame(){
+  update(locations[6]);
+}
+function restart(){
+  xp = 0;
+  health = 100;
+  gold = 50;
+  currentWeapon = 0;
+  inventory = ["stick"];
+  goldText.innerText = gold;
+  healthText.innerText = health;
+  xpText.innerText = xp;
+  goTown();
+}
+function easterEgg() {
+  update(locations[7]);
+}
+
+function pickTwo() {
+  pick(2);
+}
+
+function pickEight() {
+  pick(8);
+}
+function pick(guess) {
+  const numbers = [];
+  while (numbers.length < 10) {
+    numbers.push(Math.floor(Math.random() * 11));
+  }
+  text.innerText = "You picked " + guess + ". Here are the random numbers:\n";
+  for (let i = 0; i < 10; i++) {
+    text.innerText += numbers[i] + "\n";
+  }
+  if (numbers.includes(guess)) {
+    text.innerText += "Right! You win 20 gold!";
+    gold += 20;
+    goldText.innerText = gold;
+  } else {
+    text.innerText += "Wrong! You lose 10 health!";
+    health -= 10;
+    healthText.innerText = health;
+    if (health <= 0) {
+      lose();
+    }
+  }
 }
